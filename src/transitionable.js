0b638fecb8c0636a5283df99fe2b4d6ba137030b
Aitor Oses <aitor.oses@gmail.com>
2015-04-24 12:06:01 +0200

    Canceling requestAnimationFrame

--- src/transitionable.js
+++ src/transitionable.js
@@ -9,25 +9,35 @@ Transitionable.prototype["@@set"] = Transitionable.prototype.set;
 
 Transitionable.prototype.set = function(duration, animation, complete) {
   var self = this;
-  var open = true;
-  function recur() {
-    self["@@set"](duration, animation, function() {
-      open = false;
-      if (complete) complete();
-    });
+  this._open = true;
 
+  // Call the original set
+  self["@@set"](duration, animation, function() {
+      self._open = false;
+    if (complete) complete();
+  });
+
+  this._requestAnimation();
+}
+
+Transitionable.prototype._executeListeners = function() {
     // Execute listeners
-    if (self._listeners) {
-      self._listeners.forEach(function(fn){
-        fn(self.get());
-      })
+    if (this._listeners) {
+      this._listeners.forEach( fn => fn(this.get()) )
     }
+}
 
-    // Request frame
-    if (open) requestAnimationFrame(recur);
-
-  }
-  recur();
+Transitionable.prototype._requestAnimation = function() {
+    var self = this;
+    // Callback execution on requestAnimationFrame
+    function recur() {
+      // Request frame
+      if (self._open) {
+            self._executeListeners();
+            requestAnimationFrame(recur);
+      }
+    }
+    requestAnimationFrame(recur);
 }
 
 module.exports = Transitionable;
