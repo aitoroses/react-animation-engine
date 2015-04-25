9cd51097e70ceee923692f4da349a65aea4019b1
Aitor Oses <aitor.oses@gmail.com>
2015-04-25 14:00:10 +0200

    Created a mixin for animating state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import {PropertyAnimator} from './animator';
+// import {PropertyAnimator} from './animator';
+import TransitionableMixin from './mixin';
 import Transitionable from './transitionable';
 import Easing from 'famous/transitions/Easing';
-import {debounce, throttle} from './util/debounce';
-
 
 export {
-    throttle,
-    debounce,
-    PropertyAnimator,
+    // PropertyAnimator
+    TransitionableMixin,
     Transitionable,
     Easing
 }
---
+++ src/mixin.js
@@ -0,0 +1,58 @@
+import Transitionable from './transitionable';
+import Easing from 'famous/transitions/Easing';
+
+function TransitionableMixin(props) {
+
+  var _transitionables = {};
+
+  for (var p in props) {
+    _transitionables[p] = new Transitionable(props[p]);
+  }
+
+  var mixin = {
+    componentWillMount() {
+      for (var p in props) {
+        var trans = _transitionables[p];
+        trans.update(this._syncProp.bind(this, p))
+        this._syncProp(p);
+      }
+    },
+    componentDidMount() {
+      for (var p in props) {
+        Object.defineProperty(this, p, {
+          get() {
+            return _transitionables[p].get()
+          },
+          set(v) {
+            /* {value, duration , animation} */
+            if (typeof v != "object") {
+              v = {value: v};
+            }
+            // Store the value
+            var value = v.value;
+            delete v.value;
+            // Use 'inSine' as default curve
+            if (!v.curve) {v.curve = Easing.inSine}
+            this.tween(p, value, v);
+          }
+        });
+      }
+    },
+    getInitialState() {
+      return props;
+    },
+    _syncProp(prop) {
+      var trans = _transitionables[prop];
+      var state = {[prop]: trans.get()};
+      this.setState(state);
+    },
+    tween(prop, value, animation) {
+      var trans = _transitionables[prop];
+      trans.val(value, animation);
+    }
+  }
+
+  return mixin
+}
+
+export default TransitionableMixin;
